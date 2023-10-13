37a346cffa498bc59f89667e90f84cddebd659cb
H0neyHunter <usysstr@gmail.com>
2023-10-13 22:27:35 +0300

    Field1 Field2 Get Params DeepLink

--- app/src/main/java/com/usyssoft/deeplink/MainActivity.kt
+++ app/src/main/java/com/usyssoft/deeplink/MainActivity.kt
@@ -1,18 +1,32 @@
 package com.usyssoft.deeplink
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.usyssoft.deeplink.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var b : ActivityMainBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         b = ActivityMainBinding.inflate(layoutInflater)
         setContentView(b.root)
 
+        val getDeepLink: Uri? = intent.data
         b.apply {
+            if (getDeepLink != null && Intent.ACTION_VIEW == intent.action) {
+                val path: String? = getDeepLink.path
+                val field1Value: String? = getDeepLink.getQueryParameter("field1")
+                val field2Value: String? = getDeepLink.getQueryParameter("field2")
 
+                if (!path.isNullOrEmpty() && !field1Value.isNullOrEmpty() && !field2Value.isNullOrEmpty()) {
+                    field1TextView.text = "Field1: $field1Value"
+                    field2TextView.text = "Field2: $field2Value"
+                }
+            }
         }
 
     }
